51defe87bc0fd10a34bfbfd65bf981e3aab41161
ustavoteles <telesgustavo.dev@gmail.com>
2025-02-27 19:06:19 -0300

    Adicionando animacao, alterando fonte e botoes na pagina home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,20 @@
-import { Envelope, GithubLogo, InstagramLogo } from "@phosphor-icons/react";
+import {
+  ArrowCircleRight,
+  Envelope,
+  GithubLogo,
+  InstagramLogo,
+} from "@phosphor-icons/react";
+import { motion } from "framer-motion";
 
 function Home() {
+  const textVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <>
       <div className="-my-10 relative h-screen w-auto">
-        {" "}
         <img
           src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGJvdTNpZGFhMzJscHd5cnkzcW5ia2NmN2FlZjF3bGd6ZDNiZHdzdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d2ZfqZY5eSCR0rza/giphy.gif"
           alt="GIF de fundo"
@@ -12,38 +22,103 @@ function Home() {
         />
         <div className="items-start flex pt-20">
           <div className="flex justify-center items-start w-full h-full">
-            <div className="w-40 h-9 bg-lime-400 skew-x-24 m-5"></div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={textVariants}
+              transition={{ duration: 0.5, delay: 0.1 }}
+            >
+              {" "}
+              <div className="w-40 h-9 bg-lime-400 skew-x-24 m-5"></div>
+            </motion.div>
             <div className="flex flex-col items-center mx-4">
-              <div className="text-lime-400 text-6xl font-black italic font-['Roboto_Condensed']">
+              <motion.div
+                className="text-lime-400 text-6xl font-black italic font-['Roboto_Condensed']"
+                initial="hidden"
+                animate="visible"
+                variants={textVariants}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
                 FIT
-              </div>
-              <div className="text-lime-400 text-6xl font-black italic mb-2 font-['Roboto_Condensed']">
+              </motion.div>
+              <motion.div
+                className="text-lime-400 text-6xl font-black italic mb-2 font-['Roboto_Condensed']"
+                initial="hidden"
+                animate="visible"
+                variants={textVariants}
+                transition={{ duration: 0.5, delay: 0.4 }}
+              >
                 JOURNEY
-              </div>
+              </motion.div>
               <div className="items-center flex flex-col">
-                <div className="text-lime-400 text-1xl font-semibold italic font-[Roboto Condensed]">
+                <motion.div
+                  className="text-lime-400 text-1xl font-semibold italic font-[Roboto Condensed]"
+                  initial="hidden"
+                  animate="visible"
+                  variants={textVariants}
+                  transition={{ duration: 0.5, delay: 0.6 }}
+                >
                   PERSONAL TRAINER
-                </div>
+                </motion.div>
               </div>
             </div>
-            <div className="w-40 h-9 bg-lime-400 skew-x-24 m-5"></div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={textVariants}
+              transition={{ duration: 0.5, delay: 0.1 }}
+            >
+              {" "}
+              <div className="w-40 h-9 bg-lime-400 skew-x-24 m-5"></div>
+            </motion.div>{" "}
           </div>
         </div>
-        <div className="flex flex-col items-end justify-center my-4 h-50 mx-3">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={textVariants}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="flex flex-col items-end justify-center my-4 h-50 mx-3"
+        >
           <InstagramLogo size={32} className="text-lime-400" />
           <GithubLogo size={32} className="text-lime-400" />
           <Envelope size={32} className="text-lime-400" />
-        </div>
+        </motion.div>
+
         <div className="absolute mx-10 -my-20">
-          <p className="text-lime-400 text-5xl font-semibold italic font-['Roboto_Condensed']">
-            {" "}
-            SUA JORNADA FITNESS 
-            
-          </p>
-          <p className="mx-50 text-lime-400 text-5xl font-semibold italic font-['Roboto_Condensed']">
-            {" "}
+          <motion.p
+            className="text-lime-400 text-5xl font-semibold italic font-['Roboto_Condensed']"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            transition={{ duration: 0.5, delay: 0.8 }}
+          >
+            SUA JORNADA FITNESS
+          </motion.p>
+          <motion.p
+            className="mx-50 text-lime-400 text-5xl font-semibold italic font-['Roboto_Condensed']"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            transition={{ duration: 0.5, delay: 1.0 }}
+          >
             COMEÇA AQUI
-          </p>
+          </motion.p>
+          <motion.div
+            className="w-45 h-9 bg-lime-400 skew-x-24 mx-70 mt-5 rounded-2xl hover:scale-110"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            transition={{ duration: 0.5, delay: 2.0 }}
+          >
+            <a
+              href="#"
+              className="w-full h-full flex items-center justify-center text-black hover:text-white font-semibold gap-2"
+            >
+             INICIAR JORNADA
+              <ArrowCircleRight size={20} />
+            </a>
+          </motion.div>
         </div>
       </div>
     </>
