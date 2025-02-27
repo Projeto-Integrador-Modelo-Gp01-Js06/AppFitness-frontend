546045c8e900345a967390c2d0c75e14e803bfc5
si.gonz <you@example.com>
2025-02-27 16:44:10 -0300

    Alteração app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import DeletarCategoria from './components/categoria/deletarcategorias/DeletarCategoria'
 import FormCategoria from './components/categoria/formcategoria/FormCategoria'
+import ListarCategorias from './components/categoria/listarcategorias/ListarCategorias'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
         <div className="flex flex-col min-h-screen bg-gray-200 ">
           <div className="flex-grow">
             <Routes>
+            <Route path="/categorias" element={<ListarCategorias />} />
               <Route path="/cadastrarcategoria" element={<FormCategoria />} />
               <Route path="/editarcategoria/:id" element={<FormCategoria />} />
               <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
