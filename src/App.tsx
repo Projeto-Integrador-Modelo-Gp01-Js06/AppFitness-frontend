45039b1125ae750df454e61d02be3aee8630762e
Ana Paula <anapaula-sds@hotmail.com>
2025-02-27 17:08:33 -0300

    :sparkles: Inclusão do Navbar e Footer

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+       <Navbar />
+        <div>
+          <div className="min-h-[75vh]">
+          <Routes>
+
+          </Routes>
+          </div>
+        </div>
+        <Footer />
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App
